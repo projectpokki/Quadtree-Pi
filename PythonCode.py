ab3820cb4ee4542671af639af3e507a30c12fa63
projectpokki <158499943+projectpokki@users.noreply.github.com>
2024-02-07 10:35:13 +0800

    Update PythonCode.py

--- PythonCode.py
+++ PythonCode.py
@@ -1,6 +1,6 @@
 iterations = 10 #iterations
 
-array = [[0, 0]]
+posArray = [[0, 0]]
 nextArray = []
 area = 0
 roottwo = 2 ** 0.5
@@ -9,7 +9,7 @@ for n in range(iterations):
   sideLength = 1/(2**n)
   halfSide = sideLength * 0.5
   inRadius = 1 - (sideLength * roottwo)
-  for pos in array:
+  for pos in posArray:
     posMag = ((pos[0]**2) + (pos[1]**2)) ** 0.5
     if posMag <= inRadius:
       if pos[0] == pos[1]:
@@ -22,6 +22,6 @@ for n in range(iterations):
       nextArray.append([pos[0] + halfSide, pos[1] + halfSide])
       if pos[0] != pos[1]:
         nextArray.append([pos[0] + halfSide, pos[1]])
-  array = nextArray
+  posArray = nextArray
   nextArray = []
   print(area * 8, len(array))
