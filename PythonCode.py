fa1767a2227df54d0bdd194e58b83b815f4a68be
projectpokki <158499943+projectpokki@users.noreply.github.com>
2024-02-07 10:34:41 +0800

    Update PythonCode.py

--- PythonCode.py
+++ PythonCode.py
@@ -8,10 +8,10 @@ roottwo = 2 ** 0.5
 for n in range(iterations):
   sideLength = 1/(2**n)
   halfSide = sideLength * 0.5
-  inCircleMax = 1 - (sideLength * roottwo)
+  inRadius = 1 - (sideLength * roottwo)
   for pos in array:
     posMag = ((pos[0]**2) + (pos[1]**2)) ** 0.5
-    if posMag <= inCircleMax:
+    if posMag <= inRadius:
       if pos[0] == pos[1]:
         area += (sideLength ** 2) * 0.5
       else:
